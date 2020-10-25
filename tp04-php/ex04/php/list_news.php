0ba2519e556bbfaae289079fd5063e867524a427
Gonçalo Teixeira <skidru00@gmail.com>
2020-10-25 15:50:46 +0000

    php ex06

--- tp04-php/ex04/php/list_news.php
+++ tp04-php/ex04/php/list_news.php
@@ -1,13 +1,9 @@
 <?php
-  $db = new PDO('sqlite:news.db');
-  $stmt = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
-  FROM news JOIN
-       users USING (username) LEFT JOIN
-       comments ON comments.news_id = news.id
-  GROUP BY news.id, users.username
-  ORDER BY published DESC');
-  $stmt->execute();
-  $articles = $stmt->fetchAll();
+  include_once('database/connection.php');
+  include_once('database/news.php');
+
+  $articles = getAllNews();
+
   foreach( $articles as $article) {
     echo '<header><h1><a href=item.html?id=' . $article["id"] . '>' . $article['title'] . '</a></h1></header>';
     echo '<img src="https://www.bookdesignmadesimple.com/wp-content/uploads/2015/08/iStock_3357562_600x300.jpg" alt="">';
--- tp04-php/ex04/php/news_item.php
+++ tp04-php/ex04/php/news_item.php
@@ -1,18 +1,18 @@
 <?php 
-  $db = new PDO('sqlite:news.db');
+  include_once('database/connection.php');
+  include_once('database/comments.php');
+
   $stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = :id');
   $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
   $stmt->execute();
   $article = $stmt->fetch();
 
-  $stmt = $db->prepare('SELECT * FROM comments JOIN users USING (username) WHERE news_id = ?');
-  $stmt->execute(array($_GET['id']));
-  $comments = $stmt->fetchAll();
-
   echo '<header><h1><a href="item.html?id= ' . $article['id'] . '">' . $article['title'] . '</a></h1></header>';
   echo '<img src="https://www.bookdesignmadesimple.com/wp-content/uploads/2015/08/iStock_3357562_600x300.jpg" alt="">';
   echo '<p>' . $article['introduction'] . '</p>';
   echo '<p>' . $article['fulltext'] . '</p>';
+
+  $comments = getAllComments($_GET['id']);
 
   echo '<section id="comments">';
   echo '<h1>' . sizeof($comments) . ' Comments</h1>';
