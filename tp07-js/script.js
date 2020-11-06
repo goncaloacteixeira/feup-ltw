01fc79a0ae6a86e3130f5f7b088cb6ee49f1fe99
Gonçalo Teixeira <skidru00@gmail.com>
2020-11-06 09:37:34 +0000

    point 6 - events handler - adding rows to table

--- tp07-js/script.js
+++ tp07-js/script.js
@@ -2,4 +2,24 @@ let form = document.getElementsByTagName('form')[0]
 form.addEventListener('submit', function() {
     alert('Submitted!')
     event.preventDefault()
+
+    addNewElement()
 })
+
+function addNewElement() {
+    let inputs = document.getElementsByTagName('form')[0].elements;
+    let desc = inputs.description.value
+    let quant = inputs.quantity.value
+
+    const table = document.getElementById("products")
+    const row = table.insertRow(-1)
+
+    console.log(desc)
+
+    let x = row.insertCell(0)
+    x.innerHTML = desc
+    x = row.insertCell(1)
+    x.innerHTML = `<input value="${quant}">`
+    x = row.insertCell(2)
+    x.innerHTML = `<input type="button" value="Remove">`
+}
