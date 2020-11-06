7eb8f1db896551bb263c12f2e3da00dd080440c5
Gonçalo Teixeira <skidru00@gmail.com>
2020-11-06 09:01:02 +0000

    point 4 getting elements

--- tp07-js/script.js
+++ tp07-js/script.js
@@ -1,3 +1,10 @@
 
-let products = document.getElementById('products')
-console.log(products)
+let output = document.getElementsByTagName("form")
+console.log(output[0].outerHTML)
+
+output = document.querySelector('form input[name="quantity"]')
+console.log(output.outerHTML)
+
+output = document.querySelectorAll('form input')
+console.log(output)
+
