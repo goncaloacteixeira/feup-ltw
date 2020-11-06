682f253e7367567d1f174b76b9919b2853fba895
Gonçalo Teixeira <skidru00@gmail.com>
2020-11-06 10:00:43 +0000

    point 8 - total - updating total when changes occur

--- tp07-js/script.js
+++ tp07-js/script.js
@@ -10,18 +10,26 @@ form.addEventListener('submit', function() {
     const table = document.getElementById("products")
     const row = table.insertRow(-1)
 
-    console.log(desc)
-
     let x = row.insertCell(0)
     x.innerHTML = desc
     x = row.insertCell(1)
     x.innerHTML = `<input value="${quant}">`
+
+    x.addEventListener('change', function (event) {
+        alert('Changed!')
+        total.textContent = String(Number(total.textContent) - Number(quant) + Number(event.target.value))
+        quant = event.target.value
+    })
+
     x = row.insertCell(2)
     x.innerHTML = `<input type="button" value="Remove">`
+
+    const total = document.getElementById('total')
+    total.textContent = String(Number(total.textContent) + Number(quant))
 
     x.addEventListener('click', function () {
         alert('Removed!')
         table.deleteRow(row.rowIndex)
+        total.textContent = String(Number(total.textContent) - Number(quant))
     })
 })
-
